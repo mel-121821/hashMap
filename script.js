537d986b3c4479402bcd9e6b5a861ca84ea9ba31
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-11-28 14:52:53 -0500

    Fix if statement in set() method to ensure that multiple blocks are not evaluated in one call, add error snippet. Add an array constructor to HashMap with length = capacity and create get() method

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ class HashMap {
   constructor(loadFactor = 0.75) {
     this.capacity = 16;
     this.loadFactor = loadFactor;
+
+    // create an array with a length of capacity
+    this.buckets = new Array(this.capacity);
+    console.log(this.buckets);
+    console.log(this.buckets.length);
   }
 
   // Methods
@@ -24,31 +29,46 @@ class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + (key.charCodeAt(i) % 16);
+      hashCode = primeNumber * hashCode + (key.charCodeAt(i) % this.capacity);
       // apply modulo here to prevent integers from exceeding the max allowed value by JScript
     }
-    console.log(hashCode);
-    return hashCode;
+    return hashCode % this.capacity;
   }
 
   set(key, value) {
-    let hashCode = this.hash(key);
-    let bucket = hashCode % 16;
-    console.log(this);
+    let index = this.hash(key);
 
-    console.log(`${value} ${key} goes to bucket: ${bucket}`);
-    if (this[bucket] != undefined && this[bucket].containsKey(key)) {
-      this[bucket].changeAtKey(key, value);
-    }
-    if (this[bucket] != undefined) {
-      this[bucket].append(key, value);
+    console.log(`${value} ${key} goes to bucket: ${index}`);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
     } else {
-      this[bucket] = new LinkedList();
-      this[bucket].append(key, value);
+      if (
+        this.buckets[index] != undefined &&
+        this.buckets[index].containsKey(key)
+      ) {
+        this.buckets[index].changeAtKey(key, value);
+      } else if (this.buckets[index] != undefined) {
+        this.buckets[index].append(key, value);
+      } else {
+        this.buckets[index] = new LinkedList();
+        this.buckets[index].append(key, value);
+      }
     }
+
+    // use modulo of 16, which should return a # between 1 and 16
+    // assign key to the corresponding bucket
+    // if key exists in bucket and is === to new key, overwrite existing key
   }
 
-  get(key, value) {}
+  get(key) {
+    // return the value that is assigned to that key
+    // Pseudocode:
+    let index = this.hash(key);
+    console.log(index);
+
+    return this.buckets[index].find(key);
+  }
 
   has(key) {}
 
