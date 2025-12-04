007c9f30b7e0d8a5e9c44a04d9514cadeed63f96
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-12-04 11:12:58 -0500

    Bug found: When attempting to use get() on a non-existant element, if hashed bucket does not contain a linked list, it creates an error. Create a TODO to solve this by creating a linked list for each bucket on initialization

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ import { HashMap } from "./script.js";
 
 const test = new HashMap();
 
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
-test.set("lion", "tan");
+// test.set("apple", "red"); // bucket 10
+test.set("banana", "yellow"); // bucket 5
+test.set("carrot", "orange"); // bucket 3
+test.set("dog", "brown"); //bucket 12 (conflict!)
+test.set("elephant", "gray"); // bucket 1
+test.set("frog", "green"); // bucket 4
+test.set("grape", "purple"); // bucket 11 (conflict!)
+test.set("hat", "black"); // bucket 11 (conflict!)
+test.set("ice cream", "white"); // bucket 13
+test.set("jacket", "blue"); // bucket 14
+test.set("kite", "pink"); // bucket 15
+test.set("lion", "golden"); // bucket 12 (conflict!)
+test.set("lion", "tan"); // bucket 12 (duplicate key && conflict !)
 
-console.log(test.get("apple"));
+// console.log(test.get("apple"));
+
+// TODO: Fix undefined bucket issue
+// if the user tries to access a non-existant element with get() and the bucket does not yet contain a linked list (is undefined) it will not work.
+// Fix by creating a linked list for each bucket from the start
+
 console.log(test.get("jacket"));
 console.log(test.get("sangria"));
 
@@ -29,6 +34,9 @@ test.remove("kite");
 
 test.remove("dog");
 // test.remove("lion");
+
+test.remove("turtle");
+test.remove("apple");
 
 console.log(test);
 console.log(test.buckets);
