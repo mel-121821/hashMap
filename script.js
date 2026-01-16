a1792e49485de6808d14762df420cd79284fde38
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-01-16 10:12:11 -0500

    Remove comments, separate index out of bounds logic into its own method and change checkCap() to check_LoadFactorExceeded() for better clarity

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Note before starting:
-
-// JavaScript’s dynamic nature of arrays allows us to insert and retrieve indexes that are outside our array size range.
-
-// Example: if we create an array of size 16 to represent our buckets, nothing stops us from storing items at index 500. This defeats the purpose of limiting storage size in hash maps, so we need to enforce some restrictions.
-
-// Use the following snippet whenever you access a bucket through an index. We want to throw an error if we try to access an out-of-bounds index:
-
-// if (index < 0 || index >= buckets.length) {
-//   throw new Error("Trying to access index out of bounds");
-// }
-
 import { LinkedList } from "./linked-list.js";
 
 class HashBase {
@@ -20,11 +8,16 @@ class HashBase {
     this.buckets = this.createBuckets();
   }
 
-  // Methods
   createBuckets() {
     return Array.from({ length: this.capacity }, () => {
       return new LinkedList();
     });
+  }
+
+  check_OutOfBounds(index) {
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
   }
 
   hash(key) {
@@ -33,12 +26,11 @@ class HashBase {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + (key.charCodeAt(i) % this.capacity);
-      // apply modulo here to prevent integers from exceeding the max allowed value by JScript
     }
     return hashCode % this.capacity;
   }
 
-  checkCap() {
+  check_LoadFactorExceeded() {
     if (this.length() > this.maxLoad) {
       this.capacity *= 2;
       this.maxLoad = this.capacity * this.loadFactor;
@@ -50,21 +42,15 @@ class HashBase {
 
   has(key) {
     let index = this.hash(key);
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bounds");
-    } else {
-      return this.buckets[index].containsKey(key);
-    }
+    this.check_OutOfBounds(index);
+    return this.buckets[index].containsKey(key);
   }
 
   remove(key) {
     let index = this.hash(key);
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bounds");
-    } else {
-      this.buckets[index].removeKey(key);
-      console.log(this.buckets[index]);
-    }
+    this.check_OutOfBounds(index);
+    this.buckets[index].removeKey(key);
+    console.log(this.buckets[index]);
   }
 
   length() {
@@ -100,23 +86,16 @@ class HashSet extends HashBase {
     super(loadFactor);
   }
 
-  // Methods
   set(key) {
     let index = this.hash(key);
+    this.check_OutOfBounds(index);
     console.log(`${key} goes to bucket: ${index}`);
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bounds");
+    if (this.buckets[index].containsKey(key)) {
+      console.log(`${key} at index ${index} already exists`);
     } else {
-      if (this.buckets[index].containsKey(key)) {
-        console.log(`${key} at ${this.buckets[index]} already exists`);
-      } else {
-        // bucket.append creates a node in the linked list
-        // node will set null as the value if one is not added, so this should work without changing the linked list fn()
-        this.buckets[index].append(key);
-        if (this.checkCap()) {
-          console.log("true");
-          this.rehashKeys();
-        }
+      this.buckets[index].append(key);
+      if (this.check_LoadFactorExceeded()) {
+        this.rehashKeys();
       }
     }
   }
@@ -135,20 +114,16 @@ class HashMap extends HashBase {
     super(loadFactor);
   }
 
-  // Methods
   set(key, value) {
     let index = this.hash(key);
+    this.check_OutOfBounds(index);
     console.log(`${value} ${key} goes to bucket: ${index}`);
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bounds");
+    if (this.buckets[index].containsKey(key)) {
+      this.buckets[index].changeAtKey(key, value);
     } else {
-      if (this.buckets[index].containsKey(key)) {
-        this.buckets[index].changeAtKey(key, value);
-      } else {
-        this.buckets[index].append(key, value);
-        if (this.checkCap()) {
-          this.rehashEntries();
-        }
+      this.buckets[index].append(key, value);
+      if (this.check_LoadFactorExceeded()) {
+        this.rehashEntries();
       }
     }
   }
@@ -163,11 +138,8 @@ class HashMap extends HashBase {
 
   get(key) {
     let index = this.hash(key);
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bounds");
-    } else {
-      return this.buckets[index].find(key);
-    }
+    this.check_OutOfBounds(index);
+    return this.buckets[index].find(key);
   }
 
   values() {
